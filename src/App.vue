d8e25961b0b160cd32a2f5232aefa3db5ef44d79
Jeffrey Ernst <Jeffrey@yachtcloud.eu>
2019-10-29 22:29:18 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
     <router-view/>
   </div>
 </template>
@@ -15,6 +15,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100vh;
+  width: 100%;
 }
 
 #nav {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      bladibla
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style>
+.home {
+  background-image: url("../assets/me.jpg");
+  background-size: cover;
+  background-attachment: fixed;
+  width: 100%;
+  height: 100%;
+}
+.container {
+  background-color: white;
+  padding: 2rem;
+  width: 60%;
+}
+</style>
